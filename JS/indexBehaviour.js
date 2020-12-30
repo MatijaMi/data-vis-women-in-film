890752d22d3106161b6a8f1246af03617171a87d
MatijaMi <matijami12@gmail.com>
2020-12-30 11:40:00 +0100

    Added Protocol from last meeting, sidebar to viewer site

--- JS/indexBehaviour.js
+++ JS/indexBehaviour.js
@@ -33,3 +33,13 @@ function makeSticky() {
         navbar.classList.remove("sticky");
     }
 } 
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+} 
