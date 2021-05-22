3455b4977fb176872eff02522e875f1a36f3c047
IceBlockProduction <iceblockproduction@protonmail.com>
2021-05-22 22:24:50 +0200

    First working tooltip

--- Website/JS/timeline.js
+++ Website/JS/timeline.js
@@ -1,36 +1,40 @@
-import { select, csv, scaleLinear, 
-            max, min, scaleBand, 
-                axisTop, format, ascending, 
-                    descending } from "d3";
+import {
+    select, csv, scaleLinear,
+    max, min, scaleBand,
+    axisTop, format, ascending,
+    descending, pointer
+} from "d3";
 
-var data;
-var naiveData = [];
 const svg = select(".timeline");
 const H = svg.node().getBoundingClientRect().height;
 const W = svg.node().getBoundingClientRect().width;
 const M = 20;
 
-var yob_asc_button = document.getElementById("yob-sort-asc-button");
+var data;
+var naiveData = [];
+var tooltip = select(".tooltip");
+
+const yob_asc_button = document.getElementById("yob-sort-asc-button");
 yob_asc_button.addEventListener("click", function (event) {
     sorting(yob_asc_button);
 });
 
-var yob_desc_button = document.getElementById("yob-sort-desc-button");
+const yob_desc_button = document.getElementById("yob-sort-desc-button");
 yob_desc_button.addEventListener("click", function (event) {
     sorting(yob_desc_button);
 });
 
-var name_asc_button = document.getElementById("name-sort-asc-button");
+const name_asc_button = document.getElementById("name-sort-asc-button");
 name_asc_button.addEventListener("click", function (event) {
     sorting(name_asc_button);
 });
 
-var name_desc_button = document.getElementById("name-sort-desc-button");
+const name_desc_button = document.getElementById("name-sort-desc-button");
 name_desc_button.addEventListener("click", function (event) {
     sorting(name_desc_button);
 });
 
-var add_event_button = document.getElementById("add-event-button");
+const add_event_button = document.getElementById("add-event-button");
 add_event_button.addEventListener("click", function (event) {
     const event_text = document.getElementById("event-text");
     alert(event_text.value);
@@ -92,10 +96,6 @@ function render() {
         .range([M, H - M])
         .paddingInner(0.2);
 
-
-    const g = svg.append('g')
-        .attr('transform', `translate(0, 20)`);
-
     select("#xAxis")
         .append("svg")
         .attr("width", W)
@@ -108,13 +108,52 @@ function render() {
             .tickSize(-(H - M))
             .ticks(34));
 
+    const g = svg.append('g')
+        .attr('transform', `translate(0, 20)`);
 
     const e = g
         .selectAll("g.name")
         .data(naiveData)
         .join('g')
         .attr('class', 'name')
-        .attr('transform', d => `translate(${xScale(d.YOB)}, ${yScale(d.name) - 30})`);
+        .attr('transform', d => `translate(${xScale(d.YOB)}, ${yScale(d.name) - 30})`)
+        .on("mouseenter", (e, d) => {
+            let [x, y] = pointer(e);
+            console.log("Mouse entered");
+            console.log(x, y);
+            tooltip
+                .style("height", "50px")
+                .style("width", "50px")
+                .style("left", `${x + 350}px`)
+                .style("top", `${y + 15}px`)
+                .style("opacity", 1)
+                .style("background-color", "black")
+            /*
+            selection
+            .append("rect")
+            .attr("id", "tooltip")
+            .attr("width", 50)
+            .attr("height", 50)
+            .attr('y', 0)
+            .attr('x', d => {
+                return selection.select("rect").attr("width") / 2;
+            })
+            .attr("fill", "white")
+            .style("fill-opacity", 1)
+            .append("text")
+                .text(d => {
+                    return d.name + "\n" + d.YOB + "-" + d.YOD;
+                })
+                .style('font-size', '15px')
+                .style('font-weight', 'bold')
+                .style('opacity', '1')
+                .style('fill', 'black');
+                */
+        })
+        .on("mouseleave", function () {
+            tooltip.style("opacity", 0);
+            console.log("Mouse left");
+        });
 
     e.append('rect')
         .attr(
@@ -138,7 +177,6 @@ function render() {
         .style('font-weight', 'bold')
         .style('opacity', '1')
         .style('fill', 'white');
-
 }
 
 function getData() {
