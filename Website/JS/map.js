7c12bf3ffd45a259a3f4d0a5a4b1e5b5bce78422
Henri Dickel <Dickel@protonmail.com>
2021-07-02 12:51:37 +0200

    Resize possible

--- Website/JS/map.js
+++ Website/JS/map.js
@@ -354,3 +354,31 @@ function histogram_button_onclick(){
         window.scrollTo(0,document.body.scrollHeight);
     }
 }
+
+function CheckSizeZoom() {   
+    var minW = 1800;
+    if ($(window).width() < minW) {
+        var zoomLev = $(window).width() / minW;
+
+        if (typeof (document.body.style.zoom) != "undefined") {
+            $(document.body).css('zoom', zoomLev);
+        }
+        else {
+            // Mozilla doesn't support zoom, use -moz-transform to scale and compensate for lost width
+            $('#divWrap').css('-moz-transform', "scale(" + zoomLev + ")");
+            $('#divWrap').width($(window).width() / zoomLev + 10);
+            $('#divWrap').css('position', 'relative');
+            $('#divWrap').css('left', (($(window).width() - minW - 16) / 2) + "px");
+            $('#divWrap').css('top', "-19px");
+            $('#divWrap').css('position', 'relative');
+        }
+    }
+    else {
+        $(document.body).css('zoom', '');
+        $('#divWrap').css('position', '');
+        $('#divWrap').css('left', "");
+        $('#divWrap').css('top', "");
+        $('#divWrap').css('-moz-transform', "");
+        $('#divWrap').width("");
+    }
+}
