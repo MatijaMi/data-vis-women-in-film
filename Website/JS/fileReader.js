8ee9feb58160e56f847e1f329bc7fc3cb7ed5c19
HenriDickel <Dickel@protonmail.com>
2022-02-05 15:39:28 +0100

    Added FileReader Class
    - Also used it for pioneerOfTheDay

---
+++ Website/JS/fileReader.js
@@ -0,0 +1,71 @@
+class Pioneer {
+    constructor(id, name, aka, link, image_url, worked_as, worked_in, birth_date, death_date) {
+        this.id = id;
+        this.name = name;
+        this.aka = aka;
+        this.link = link;
+        this.image_url = image_url;
+        this.worked_as = worked_as;
+        this.worked_in = worked_in;
+        this.birth_date = birth_date;
+        this.death_date = death_date;
+    }
+
+    toString = function pioneerToString() {
+        return this.name;
+    }
+
+}
+
+function readCSVFile(path) {
+
+    //Creat XML Request
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", path, false);
+    rawFile.send(null);
+
+    //Busy loop
+    while (!rawFile.DONE) { }
+
+    //Return filecontent
+    return rawFile.responseText;
+}
+
+
+function getAllPioneers() {
+
+    //Result
+    var pioneers = [];
+
+    //Read File
+    fileContent = readCSVFile("Data/map_pioneer_data.csv");
+
+    //Rows
+    rows = fileContent.split('\r\n');
+
+    //Foreach Row
+    for (var i = 1; i < rows.length; i++) {
+
+        //Row fields
+        var data = rows[i].split(',');
+
+        //Extract values
+        var id = data[0];
+        var name = data[1];
+        var aka = data[2].split('|');
+        var link = data[3];
+        var image_url = data[4];
+        var worked_as = data[5].split('|');;
+        var worked_in = data[6].split('|');;
+        var birth_date = new Date(data[7] + ", " + data[8]);
+        var death_date = new Date(data[9] + ", " + data[10]);
+
+        //Create pioneer
+        pioneers[i - 1] = new Pioneer(id, name, aka, link, image_url, worked_as, worked_in, birth_date, death_date);
+    }
+
+    //Return
+    console.log(pioneers);
+    return pioneers;
+}
+
--- Website/JS/pioneerOfTheDay.js
+++ Website/JS/pioneerOfTheDay.js
@@ -1,64 +1,43 @@
-//All pioneers with birthdays on this day
-var pioneers = [];
-
 //Function that finds the pioneer of the current day for main page
 function getPioneerOfTheDay() {
-    //Load data
-    d3.queue().defer(d3.csv, "Data/data_modified.csv", function (row) {
-            const dob = String(row.DOB) // Get the Day of Birth for the current pioneer
-            const mob = String(row.MOB) // Get the Month of Birth for the current pioneer
-            const born = "" + mob + " " + dob // Format Date of Birth
 
-            const date = new Date() // Get today Date
-            const month = date.toLocaleString('en-us', { month: 'long' }) // Extract the month in long format
-            const day = date.getDate() // Extract the day as number
-            const today = "" + month + " " + day // Format String the same as it is in the data
-            //const debug_today1 = "February 10" // For showcasing if there is no Pioneer born on day of presentation
-            //const debug_today2 = "February 11"
-            if (born == today) {
-                pioneers.push(row);
-            }
-        }).await(choosePioneer);
-}
+    //Data
+    var pioneers = getAllPioneers();
 
-function choosePioneer(){
-        //If there is a pioneer
-        if (pioneers.length > 0) {
-            const rand = Math.floor(Math.random() * pioneers.length);
-            const element = pioneers[rand];
-            try {
-                document.getElementById("birthday_title").innerText = "Happy Birthday: " + element.name
-                document.getElementById("description").hidden = true
-                document.getElementById("lived").innerHTML = "<b>Lived</b>:<br>" + element.MOB + " " + element.DOB + ", " + +element.YOB + " - " + element.MOD + " " + element.DOD + ", " + +element.YOD
-                var worked = String(element.worked_in)
-                worked = worked.split("|").join(", ")
-                document.getElementById("worked").innerHTML = "<b>Worked in</b>:<br>\n" + worked
-                var aka = String(element.aka)
-                aka = aka.split("|").join(", ")
-                document.getElementById("aka").innerHTML = "<b>Also known as</b>:<br>\n" + aka
+    //Filter
+    pioneers = pioneers.filter(pioneer => new Date().getDate() == pioneer.birth_date.getDate() && new Date().getMonth() == pioneer.birth_date.getMonth());
 
-                //Try to load image
-                const img_url = new URL(element.image_url)
-                document.getElementById("birthday_pic").src = element.image_url
-                document.getElementById("birthday_pic").style.display = "inline-block";
-            } catch (error) {
-                //console.log(error)
-                //console.log("No image found for todays pioneer")
-                //document.getElementById("pod_title").innerText = "Birthday: " + element.name
-                
-                document.getElementById("birthday_pic").src = "Images/Unknown.jpg"
-                document.getElementById("birthday_pic").style.display = "inline-block";
-            } finally {
-                var button = document.getElementById("birthday_contribute_button")
-                button.style.display = "none"
-                button.addEventListener("click", function (event) {
-                    window.open(element.link)
-                })
-            }
-        } 
-        //There is no pioneer
-        else {
-            document.getElementById("birthday_contribute_button").style.display = "";
+    //No birthdays
+    if (pioneers.length == 0) {
+        //Don't show contribute button
+        document.getElementById("birthday_contribute_button").style.display = "";
+    }
+    else {
+        //Get random pioneer in list
+        const rand = Math.floor(Math.random() * pioneers.length);
+        const pioneer = pioneers[rand];
+
+        try {
+            //Set values
+            document.getElementById("birthday_title").innerText = "Happy Birthday: " + pioneer.name;
+            document.getElementById("description").hidden = true;
+            document.getElementById("lived").innerHTML = "<b>Lived</b>:<br>" + pioneer.birth_date.toDateString() + " - " + pioneer.death_date.toDateString();
+            document.getElementById("worked").innerHTML = "<b>Worked in</b>:<br>\n" + pioneer.worked_in.join(", ");
+            document.getElementById("aka").innerHTML = "<b>Also known as</b>:<br>\n" + pioneer.aka.join(", ");
+            document.getElementById("birthday_pic").src = pioneer.image_url;
+            document.getElementById("birthday_pic").style.display = "inline-block";
+        } catch (error) {
+            //Error setting values
+            console.log("Error setting pioneer of the day values " + error);
+            document.getElementById("birthday_pic").src = "Images/Unknown.jpg";
+            document.getElementById("birthday_pic").style.display = "inline-block";
+        } finally {
+            //Link button
+            var button = document.getElementById("birthday_contribute_button");
+            button.style.display = "none";
+            button.addEventListener("click", function (event) {
+                window.open(element.link);
+            })
         }
+    }
 }
-
